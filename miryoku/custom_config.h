a7899d79a2fe15a49d92453991ed0cb05912b933
Iakov Davydov <671660+idavydov@users.noreply.github.com>
2024-05-08 16:23:21 +0200

    make sure miryoku nav layer is set

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,7 +1,7 @@
 #define MIRYOKU_ALPHAS_QWERTY
 #define MIRYOKU_NAV_VI
 
-#define MIRYOKU_ALTERNATIVES_MEDIA_VI \
+#define MIRYOKU_LAYER_NAV \
 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              MAGIC_TOGGLE_ALT_GUI,  MAGIC_UNSWAP_ALT_GUI,  MAGIC_UNSWAP_ALT_GUI,        &u_rgb_bri,        &u_rgb_tog,        \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp C_PREV,            &kp C_VOL_DN,          &kp C_VOL_UP,                &kp C_NEXT,        &u_ep_tog,         \
 U_NA,              &kp RALT,          &u_to_U_FUN,       &u_to_U_MEDIA,     U_NA,              &u_bt_sel_0,           &u_bt_sel_1,           &u_bt_sel_2,                 &u_bt_sel_3,       &u_out_tog,        \
