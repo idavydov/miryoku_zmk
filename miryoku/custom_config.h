35649ab5680a8242a5866f39649d1e0a4704a107
Iakov Davydov <671660+idavydov@users.noreply.github.com>
2024-05-08 16:06:38 +0200

    custom config: swap gui/ctrl + qwerty & vi

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,8 @@
-// Copyright 2021 Manna Harbour
-// https://github.com/manna-harbour/miryoku
+#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_NAV_VI
 
+#define MIRYOKU_ALTERNATIVES_MEDIA_VI \
+U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              MAGIC_TOGGLE_ALT_GUI,  MAGIC_UNSWAP_ALT_GUI,  MAGIC_UNSWAP_ALT_GUI,        &u_rgb_bri,        &u_rgb_tog,        \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp C_PREV,            &kp C_VOL_DN,          &kp C_VOL_UP,                &kp C_NEXT,        &u_ep_tog,         \
+U_NA,              &kp RALT,          &u_to_U_FUN,       &u_to_U_MEDIA,     U_NA,              &u_bt_sel_0,           &u_bt_sel_1,           &u_bt_sel_2,                 &u_bt_sel_3,       &u_out_tog,        \
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp C_STOP,            &kp C_PP,              &kp C_MUTE,                  U_NP,              U_NP
